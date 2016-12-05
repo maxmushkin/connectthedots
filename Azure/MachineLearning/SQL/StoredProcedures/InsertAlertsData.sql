03a43ce56ee32e118ea56eeb3adbf6e494fc8cd2
maksim.mushkin <maksim.mushkin@akvelon.com>
2016-12-05 12:54:51 +0200

    cleanup solution

--- Azure/MachineLearning/SQL/StoredProcedures/InsertAlertsData.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[InsertAlertsData]
-	@dataList [dbo].[AlertsDataTableType] READONLY
-AS
-	insert into [dbo].[AlertsData] ([value], [guid], [displayname], [organization], [unitofmeasure], [measurename], [location], [timecreated])
-		select [value], [guid], [displayname], [organization], [unitofmeasure], [measurename], [location], [timecreated] from @dataList
-RETURN 0
